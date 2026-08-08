daa659b0f1608fad4ae0b3d201658179b73964b8
Shruutea <94055233+Shruutea@users.noreply.github.com>
2026-08-08 13:09:58 +0530

    Time: 4 ms (19.30%) | Memory: 28.2 MB (73.74%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,9 +2,20 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        std::sort(nums.begin(),nums.end());
-        int mid = (n-1)/2;
+        int a = nums[0];
+        int count = 0;
+        for (int i =0 ; i<n; i++){
+            if (nums[i]==a){
+                count ++;
+            } else {
+                count--;
+            }
+            if (count==0){
+                a = nums[i];
+                count = 1;
+            }
+        }
 
-        return nums[mid];
+        return a ;
     }
 };
