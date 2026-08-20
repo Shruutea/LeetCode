1d9eb3109160bf0397d6ca9c8f38145e7cb4a05a
Shruutea <94055233+Shruutea@users.noreply.github.com>
2026-08-20 18:51:01 +0530

    Time: 0 ms (100.00%) | Memory: 87.1 MB (45.37%) - LeetSync

--- 790-global-and-local-inversions/global-and-local-inversions.cpp
+++ 790-global-and-local-inversions/global-and-local-inversions.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isIdealPermutation(vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) {
-            if (abs(nums[i] - i) > 1) {
+            if (i - nums[i] > 1 || i - nums[i] < -1) {
                 return false;
             }
         }
