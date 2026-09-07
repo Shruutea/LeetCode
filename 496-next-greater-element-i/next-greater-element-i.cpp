f9b47788222920a270ef309a7e92877732d62d63
Shruutea <94055233+Shruutea@users.noreply.github.com>
2026-09-07 12:38:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (79.87%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,9 +3,9 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int> map;
         int n = nums2.size();
-        for (int i = 0; i<n ;i++){
-            map.insert({nums2[i],-1});
-        }
+        // for (int i = 0; i<n ;i++){
+        //     map.insert({nums2[i],-1});
+        // }
 
         stack<int> st;
         for (int i = 0;i<n ;i++){
@@ -16,7 +16,7 @@ public:
                 }
                 if (nums2[i]>nums2[st.top()]){
                     int idx = st.top();
-                    map.find(nums2[idx])->second = nums2[i];
+                    map.insert({nums2[idx],nums2[i]});
                     st.pop();
                 } else {
                     st.push(i);
